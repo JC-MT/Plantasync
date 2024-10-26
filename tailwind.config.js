d13c1d8afbc6f2c20e48b44c01ce41c04f6ae92f
Jan Matias <jan@thebeyondgroup.la>
2024-10-26 09:29:54 -0400

    chore: update dev config files - Add new build folders in gitignore - Add JIT mode on tailwind config - Add remix to TS config and compiler options

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: 'jit',
+  content: ['./app/root.tsx', './app/**/*.{ts,tsx,js,jsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
