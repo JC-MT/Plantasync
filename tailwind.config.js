2f31b711f2ba7256540fafb959a140b294fdd2b1
Jan Matias <jan@thebeyondgroup.la>
2024-11-23 14:33:01 -0500

    init: add remix app cloudflare template

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  mode: 'jit',
-  content: ['./app/root.tsx', './app/**/*.{ts,tsx,js,jsx}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-};
