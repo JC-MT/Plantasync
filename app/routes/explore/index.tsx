4a17dbbc98e52dc3ad869223f944d74a0116754c
Jan Matias <jan@thebeyondgroup.la>
2025-04-30 10:21:04 -0400

    chore: add basic frontend routes

---
+++ app/routes/explore/index.tsx
@@ -0,0 +1,10 @@
+export function meta() {
+  return [
+    { title: "Explore Index Page" },
+    { name: "description", content: "This is the Explore Index page" }
+  ];
+}
+
+export default function Index() {
+  return <header>Hello World from Explore Index</header>;
+}
---
+++ app/routes/plants/index.tsx
@@ -0,0 +1,10 @@
+export function meta() {
+  return [
+    { title: "Index Page" },
+    { name: "description", content: "This is the Index page" }
+  ];
+}
+
+export default function Index() {
+  return <header>Hello World from Index</header>;
+}
