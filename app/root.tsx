a29c7f66c86c5c67a1c5546808f91e91b61bb9ed
Jan Matias <jan@thebeyondgroup.la>
2025-04-30 10:25:02 -0400

    chore: add basic layout components

--- app/root.tsx
+++ app/root.tsx
@@ -4,10 +4,12 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration
 } from "react-router";
 
 import type { Route } from "./+types/root";
+import { Footer } from "./layout/Footer";
+import { Navbar } from "./layout/Navbar";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
@@ -15,12 +17,12 @@ export const links: Route.LinksFunction = () => [
   {
     rel: "preconnect",
     href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
+    crossOrigin: "anonymous"
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+  }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -33,7 +35,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <Navbar />
         {children}
+        <Footer />
         <ScrollRestoration />
         <Scripts />
       </body>
