f3756d1404d9cb89796357f0218cfe607151e86c
Jan Matias <jan@thebeyondgroup.la>
2025-05-10 23:35:22 -0400

    chore: add auth layout and remove about page

--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -1,11 +1,14 @@
 export function meta() {
-    return [
-      { title: "Log-In Page" },
-      { name: "description", content: "This is the Log-In page" }
-    ];
-  }
-  
-  export default function LogIn() {
-    return <header>Hello World from Log-In</header>;
-  }
-  
+  return [
+    { title: "Log-In Page" },
+    { name: "description", content: "This is the Log-In page" }
+  ];
+}
+
+export default function LogIn() {
+  return (
+    <div>
+      <h1>Sign in to your account</h1>
+    </div>
+  );
+}
--- app/routes/auth/register.tsx
+++ app/routes/auth/register.tsx
@@ -1,11 +1,14 @@
 export function meta() {
-    return [
-      { title: "Register Page" },
-      { name: "description", content: "This is the Register page" }
-    ];
-  }
-  
-  export default function Register() {
-    return <header>Hello World from Register</header>;
-  }
-  
+  return [
+    { title: "Register Page" },
+    { name: "description", content: "This is the Register page" }
+  ];
+}
+
+export default function Register() {
+  return (
+    <div>
+      <h1>Create account</h1>
+    </div>
+  );
+}
