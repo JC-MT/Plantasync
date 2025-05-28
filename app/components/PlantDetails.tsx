a578e5d2a8644f85571e279ba20a5ad195dd8c61
Jan Matias <jan@thebeyondgroup.la>
2025-05-28 00:12:47 -0400

    style: add dummy text and styles to details page

--- app/components/PlantDetails.tsx
+++ app/components/PlantDetails.tsx
@@ -1,12 +1,13 @@
 const { VITE_IMAGE_CDN_URL } = import.meta.env;
-import { Image } from "./Image";import { Link } from "react-router";
+import { Image } from "./Image";
+import { Link } from "react-router";
 import PlantHistory from "~/components/PlantHistory";
 
 export default function PlantDetails({ plant }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-[40%_auto] gap-2 sm:gap-10 md:gap-16">
       <Image
-        imageUrl={`${VITE_IMAGE_CDN_URL}default_plant_image.jpg?v=1746612628`}
+        imageUrl="default_plant_image.jpg?v=1746612628"
         classNames="w-full object-cover bg-plant-card border border-zinc-200 rounded-lg sticky top-20"
         loading="eager"
         sizes="(min-width: 768px) 42vw, 100vw"
@@ -16,7 +17,7 @@ export default function PlantDetails({ plant }) {
         height={1000}
         viewTransition={plant.id}
       />
-      <div className="grid grid-cols-1 gap-3">
+      <div className="grid grid-cols-1 gap-4">
         <div className="tracking-tight text-balance">
           <h1
             className="text-3xl/none md:text-5xl font-bold"
@@ -28,20 +29,37 @@ export default function PlantDetails({ plant }) {
           </h1>
           <p className="text-base font-medium">{plant.latin}</p>
         </div>
-        <div className="flex flex-col p-3 rounded-md border border-zinc-200 shadow-xs">
+        <div className="flex flex-col gap-2 p-4 rounded-md border border-zinc-200 bg-white">
           <h2 className="font-bold text-3xl/none md:text-4xl/none mb-1">
-            Todo
+            Action Items
           </h2>
           <h3 className="pb-1 font-bold text-xl/none md:text-2xl/none border-b border-zinc-200">
             Today
           </h3>
+          <ul className="flex flex-col gap-1">
+            <li className="text-left text-base/none font-semibold text-dark-green">
+              Water Plant
+            </li>
+            <li className="text-left text-base/none font-semibold text-dark-green">
+              Rotate Vase
+            </li>
+          </ul>
           <h3 className="pb-1 font-bold text-xl/none md:text-2xl/none border-b border-zinc-200">
             Upcoming
           </h3>
-          <div className="flex flex-wrap w-full text-center gap-1">
+
+          <ul className="flex flex-col gap-1">
+            <li className="text-left text-base/none font-semibold text-dark-green">
+              Water 3 days
+            </li>
+            <li className="text-left text-base/none font-semibold text-dark-green">
+              Rotate vase 3 days
+            </li>
+          </ul>
+          <div className="flex flex-wrap w-full text-center gap-1 mt-3">
             <button
               type="button"
-              className="flex items-center justify-center gap-1 bg-dark-green/5 hover:bg-dark-green hover:text-light-green px-4 py-1 font-medium text-sm lg:text-base rounded-full hover:cursor-pointer"
+              className="flex items-center justify-center gap-1 border border-zinc-200 bg-dark-green/5 hover:bg-dark-green hover:text-light-green px-4 py-1 font-medium text-sm lg:text-base rounded-full hover:cursor-pointer"
             >
               <span>Water Plant</span>
               <img
@@ -52,7 +70,7 @@ export default function PlantDetails({ plant }) {
             </button>
             <button
               type="button"
-              className="flex items-center justify-center gap-1 bg-dark-green/5 hover:bg-dark-green hover:text-light-green px-4 py-1 font-medium text-sm lg:text-base rounded-full hover:cursor-pointer"
+              className="flex items-center border border-zinc-200 justify-center gap-1 bg-dark-green/5 hover:bg-dark-green hover:text-light-green px-4 py-1 font-medium text-sm lg:text-base rounded-full hover:cursor-pointer"
             >
               <span>Add Plant</span>
               <img
@@ -62,28 +80,25 @@ export default function PlantDetails({ plant }) {
               />
             </button>
             <Link
-              className="bg-dark-green/5 hover:bg-dark-green hover:text-light-green px-4 py-1 font-medium text-sm lg:text-base rounded-full"
-              to="/plants"
+              to={-1}
+              className="bg-dark-green/5 hover:bg-dark-green border border-zinc-200 hover:text-light-green px-4 py-1 font-medium text-sm lg:text-base rounded-full"
               viewTransition
             >
               Go Back
             </Link>
           </div>
         </div>
-        <div className="flex flex-col p-3 rounded-md border border-zinc-200 shadow-xs">
+        <div className="flex flex-col p-4 rounded-md border border-zinc-200 bg-white">
           <h2 className="font-bold text-3xl/none md:text-4xl/none">
-            Care info
+            Plant Information
           </h2>
           <p
             className={`${
-              !plant.last_water || plant.needsWater
-                ? "animate-[pulse_1s_ease-in-out_infinite] text-red-400"
-                : ""
+              !plant.last_water || plant.needsWater ? "text-red-400" : ""
             }`}
           >
-            <strong>Last Watered:</strong>
-            Has never been watered. Please edit the date or press the water
-            plant button to water today.
+            <strong>Last Watered: </strong>
+            Unkhown
           </p>
           <p>
             <strong>Category:</strong> {plant.category}
@@ -92,10 +107,10 @@ export default function PlantDetails({ plant }) {
             <strong>Origin:</strong> {plant.origin}
           </p>
           <p>
-            <strong>Ideal Light:</strong> {plant.ideal_light}
+            <strong>Ideal Light:</strong> {plant.ideallight}
           </p>
           <p>
-            <strong>Water Tips:</strong> {plant.ideal_watering}
+            <strong>Water Tips:</strong> {plant.watering}
           </p>
         </div>
         <PlantHistory
