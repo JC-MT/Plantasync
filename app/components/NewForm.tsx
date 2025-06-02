df6dd7d1440cda4ea21c2a74976fd4a579477af6
Jan Matias <jan@thebeyondgroup.la>
2025-06-02 09:13:07 -0400

    wip

---
+++ app/components/NewForm.tsx
@@ -0,0 +1,231 @@
+"use client";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { cn } from "../lib/utils";
+import { Button } from "../components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "../components/ui/form";
+import { Input } from "../components/ui/input";
+import { format } from "date-fns";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from "../components/ui/popover";
+import { Calendar } from "../components/ui/calendar";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { RadioGroup, RadioGroupItem } from "../components/ui/radio-group";
+import { CloudUpload, Paperclip } from "lucide-react";
+import {
+  FileInput,
+  FileUploader,
+  FileUploaderContent,
+  FileUploaderItem
+} from "../components/ui/file-upload";
+
+const formSchema = z.object({
+  name_7236630084: z.string().min(1),
+  name_8824035486: z.string().min(1).optional(),
+  name_7802436738: z.coerce.date(),
+  name_2316592808: z.string(),
+  name_3570215844: z.string().optional()
+});
+
+export default function MyForm() {
+  const [files, setFiles] = useState<File[] | null>(null);
+
+  const dropZoneConfig = {
+    maxFiles: 5,
+    maxSize: 1024 * 1024 * 4,
+    multiple: true
+  };
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name_7802436738: new Date()
+    }
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      console.log(values);
+    } catch (error) {
+      console.error("Form submission error", error);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 max-w-3xl mx-auto py-10"
+      >
+        <FormField
+          control={form.control}
+          name="name_7236630084"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" type="" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="name_8824035486"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" type="" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="name_7802436738"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Date of birth</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                Your date of birth is used to calculate your age.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="name_2316592808"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Gender</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  className="flex flex-col space-y-1"
+                >
+                  {[
+                    ["Male", "male"],
+                    ["Female", "female"],
+                    ["Other", "other"]
+                  ].map((option, index) => (
+                    <FormItem
+                      className="flex items-center space-x-3 space-y-0"
+                      key={index}
+                    >
+                      <FormControl>
+                        <RadioGroupItem value={option[1]} />
+                      </FormControl>
+                      <FormLabel className="font-normal">{option[0]}</FormLabel>
+                    </FormItem>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+              <FormDescription>Select your gender</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="name_3570215844"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Select File</FormLabel>
+              <FormControl>
+                <FileUploader
+                  value={files}
+                  onValueChange={setFiles}
+                  dropzoneOptions={dropZoneConfig}
+                  className="relative bg-background rounded-lg p-2"
+                >
+                  <FileInput
+                    id="fileInput"
+                    className="outline-dashed outline-1 outline-slate-500"
+                  >
+                    <div className="flex items-center justify-center flex-col p-8 w-full ">
+                      <CloudUpload className="text-gray-500 w-10 h-10" />
+                      <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+                        <span className="font-semibold">Click to upload</span>
+                        &nbsp; or drag and drop
+                      </p>
+                      <p className="text-xs text-gray-500 dark:text-gray-400">
+                        SVG, PNG, JPG or GIF
+                      </p>
+                    </div>
+                  </FileInput>
+                  <FileUploaderContent>
+                    {files &&
+                      files.length > 0 &&
+                      files.map((file, i) => (
+                        <FileUploaderItem key={i} index={i}>
+                          <Paperclip className="h-4 w-4 stroke-current" />
+                          <span>{file.name}</span>
+                        </FileUploaderItem>
+                      ))}
+                  </FileUploaderContent>
+                </FileUploader>
+              </FormControl>
+              <FormDescription>Select a file to upload.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
--- app/routes/plants/add.tsx
+++ app/routes/plants/add.tsx
@@ -1,10 +1,23 @@
+import { Form } from "react-router";
+import { InputForm } from "../../components/InputForm";
+import PageContainer from "~/layout/PageContainer.js";
+
 export function meta() {
   return [
-    { title: "Add Page" },
-    { name: "description", content: "This is the add page" }
+    { title: "Plantasync — Add a new plant" },
+    { name: "description", content: "Add a new plant to your garden" }
   ];
 }
 
 export default function Add() {
-  return <header>Hello World from Add</header>;
+  return (
+    <PageContainer>
+      <h1 className="text-3xl/none md:text-5xl font-bold tracking-tight">
+        Add a new plant
+      </h1>
+      <Form action="/add" method="post">
+        <InputForm />
+      </Form>
+    </PageContainer>
+  );
 }
