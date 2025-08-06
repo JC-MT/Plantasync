756ec88d599fceb90fbd6cf492af24b46eec5997
Jan Matias <jan@thebeyondgroup.la>
2025-08-06 10:40:57 -0400

    chore: add new shared types and constants files

---
+++ app/constants.ts
@@ -0,0 +1,47 @@
+export const climates = [
+  {
+    value: "tropical",
+    label: "Tropical"
+  },
+  {
+    value: "temperate",
+    label: "Temperate"
+  },
+  {
+    value: "dry",
+    label: "Dry"
+  },
+  {
+    value: "humid",
+    label: "Humid"
+  },
+  {
+    value: "subtropical",
+    label: "Subtropical"
+  },
+  {
+    value: "continental",
+    label: "Continental"
+  }
+];
+
+export const lights = [
+  {
+    value: "direct",
+    label: "Direct"
+  },
+  {
+    value: "partial",
+    label: "Partial"
+  },
+  {
+    value: "indirect",
+    label: "Indirect"
+  },
+  {
+    value: "shade",
+    label: "Shade"
+  }
+];
+
+export const FILTERED_PLANT_INCREMENT = 10;
