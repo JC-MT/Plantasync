5b14c7c5c6e6e3cc97cb9a156bdd4bc29a6a751a
Jan Matias <jan@thebeyondgroup.la>
2025-12-01 12:29:27 -0500

    feat: simplify plant page layout and add pagination queries for performance

--- app/routes/explore/plant.tsx
+++ app/routes/explore/plant.tsx
@@ -1,6 +1,13 @@
-import PlantDetails from "~/components/PlantDetails.js";
-import { getData } from "../../db/query.js";
+import { getData } from "~/db/query.js";
+import { Slider } from "~/components/Slider.js";
+import { Image } from "~/components/Image.js";
+const { VITE_IMAGE_CDN_URL } = import.meta.env;
+import { defaultSliderPlants } from "~/constants";
+import { InfoCard } from "~/components/InfoCard.js";
+import { SliderCard } from "~/components/SliderCard.js";
 import PageContainer from "~/layout/PageContainer.js";
+import { PlantDetails } from "~/components/PlantDetails.js";
+import type { Plant } from "~/components/types/SharedTypes.js";
 
 export async function loader({
   params
@@ -21,10 +28,50 @@ export function meta({ data }: { data: any }) {
   ];
 }
 
-export default function Detail({ loaderData }: { loaderData: any }) {
+export default function Detail({ loaderData }: { loaderData: Plant }) {
   return (
     <PageContainer>
-      <PlantDetails plant={loaderData} />
+      <div className="grid grid-cols-1 sm:grid-cols-[40%_auto] gap-2 sm:gap-5 md:gap-10 lg:gap-16">
+        <Image
+          imageUrl="default_plant_image.jpg?v=1746612628"
+          classNames="w-full object-cover bg-plant-card border border-zinc-200 rounded-lg sticky top-20"
+          loading="eager"
+          sizes="(min-width: 768px) 42vw, 100vw"
+          alt="Stock plant image"
+          isHero={true}
+          width={1000}
+          height={1000}
+          viewTransition={loaderData.id}
+        />
+        <div className="flex flex-col gap-4">
+          <div className="tracking-tight text-balance">
+            <h1
+              className="text-3xl/none md:text-5xl font-bold"
+              style={{
+                viewTransitionName: `plant-title-${loaderData.id}`
+              }}
+            >
+              {loaderData.name}
+            </h1>
+            <p className="text-base font-medium">{loaderData.category}</p>
+          </div>
+          <InfoCard>
+            <PlantDetails plant={loaderData} />
+          </InfoCard>
+          <InfoCard hasSlider={true}>
+            <Slider title="Most Popular Plants">
+              {defaultSliderPlants.map((plant, index) => (
+                <SliderCard
+                  key={index}
+                  imageUrl={`${VITE_IMAGE_CDN_URL}default_plant_image.jpg?v=1746612628`}
+                  name={plant.name}
+                  description={plant.origin}
+                />
+              ))}
+            </Slider>
+          </InfoCard>
+        </div>
+      </div>
     </PageContainer>
   );
 }
--- app/routes/plants/plant.tsx
+++ app/routes/plants/plant.tsx
@@ -1,42 +1,146 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router";
-import { useGardenData } from "~/hooks/useGardenData.js";
-import PlantDetails from "~/components/PlantDetails.js";
+import { Image } from "~/components/Image.js";
+import { InfoCard } from "~/components/InfoCard.js";
+import { PlantTasks } from "~/components/PlantTasks";
 import PageContainer from "~/layout/PageContainer.js";
-import { getData } from "../../db/query.js";
+import { useGardenData } from "~/hooks/useGardenData.js";
+import { PlantDetails } from "~/components/PlantDetails.js";
+import { PlantHistory } from "~/components/PlantHistory.js";
+import { getData, postData, updateData } from "~/db/query.js";
+import type { Action, Plant } from "~/components/types/SharedTypes.js";
 
-export async function loader({
-  params
+export function meta({
+  data
 }: {
-  params: Record<string, string | undefined>;
+  data: { plant: Plant; actions: { actions: Action[]; count: number } };
 }) {
-  const plant = await getData(`garden?id=eq.${params.id}`);
-  if (!plant) {
-    throw new Response("Failed to get plants:", { status: 500 });
-  }
-  return plant[0];
-}
-
-export function meta({ data }: { data: any }) {
   return [
-    { title: `Plantasync — ${data?.name || "Plant"} details` },
+    { title: `Plantasync — ${data?.plant?.name || "Plant"} details` },
     { name: "description", content: "This is the Detail page" }
   ];
 }
 
-export default function Detail({ loaderData }: { loaderData: any }) {
+export async function action({ request }: { request: Request }) {
+  const formData = await request.formData();
+  const actionType = formData.get("type") as string;
+  const plantId = formData.get("plant_id") as string;
+  const actionKey = formData.get("key") as string;
+
+  try {
+    const action = await postData("actions", {
+      plant_id: plantId,
+      type: actionType
+    });
+
+    if (action[0]) {
+      try {
+        const plant = await updateData(`garden?id=eq.${plantId}`, {
+          [actionKey]: new Date().toISOString()
+        });
+
+        return plant[0];
+      } catch (error) {
+        throw new Response("Failed to update plant:", {
+          status: 500,
+          statusText: String(error)
+        });
+      }
+    }
+
+    throw new Response("Action creation failed", { status: 500 });
+  } catch (error) {
+    throw new Response("Failed to add action:", {
+      status: 500,
+      statusText: String(error)
+    });
+  }
+}
+
+export async function loader({
+  params,
+  request
+}: {
+  params: { id: number };
+  request: Request;
+}) {
+  const searchParams = new URL(request.url).searchParams;
+  const page = Number(searchParams.get("page")) || 1;
+  const actionsPromise = getData(
+    `actions?plant_id=eq.${params.id}${searchParams.get("filter") ? `&type=eq.${searchParams.get("filter")}` : ""}&limit=${page * 5}&offset=${page > 1 ? (page - 1) * 5 : 0}`,
+    { estimatedCount: true }
+  )
+    .then((result) => {
+      return { results: result.data, count: result.count };
+    })
+    .catch((error) => {
+      console.error("Failed to get actions:", error);
+      throw new Response("Failed to get actions:", { status: 500 });
+    });
+
+  const plantData = await getData(`garden?id=eq.${params.id}`);
+  if (!plantData || !plantData[0]) {
+    throw new Response("Failed to get plant:", { status: 500 });
+  }
+
+  return { plant: plantData[0], actions: actionsPromise };
+}
+
+export default function Detail({
+  loaderData
+}: {
+  loaderData: {
+    plant: Plant;
+    actions: Promise<{ results: Action[]; count: number }>;
+  };
+}) {
+  const { plant, actions } = loaderData;
   const location = useLocation();
   const { fetchGardenData } = useGardenData();
-  
+
   useEffect(() => {
-    if(location.state?._isRedirect) {
+    if (location.state?._isRedirect) {
       fetchGardenData();
     }
   }, []);
-  
+
   return (
     <PageContainer>
-      <PlantDetails plant={loaderData} />
+      <div className="grid grid-cols-1 sm:grid-cols-[40%_auto] gap-2 sm:gap-5 md:gap-10 lg:gap-16">
+        <Image
+          imageUrl="default_plant_image.jpg?v=1746612628"
+          classNames="w-full object-cover bg-plant-card border border-zinc-200 rounded-lg sticky top-20"
+          loading="eager"
+          sizes="(min-width: 768px) 42vw, 100vw"
+          alt="Stock plant image"
+          isHero={true}
+          width={1000}
+          height={1000}
+          viewTransition={plant.id}
+        />
+        <div className="grid gap-4">
+          <div className="tracking-tight text-balance">
+            <h1
+              className="text-3xl/none md:text-5xl font-bold"
+              style={{
+                viewTransitionName: `plant-title-${plant.id}`
+              }}
+            >
+              {plant.name}
+            </h1>
+            <p className="text-base font-medium">{plant.category}</p>
+          </div>
+          <InfoCard>
+            <PlantTasks plant={plant} />
+          </InfoCard>
+          <InfoCard>
+            <PlantDetails plant={plant} />
+          </InfoCard>
+          <InfoCard>
+            <PlantHistory actions={actions} />
+          </InfoCard>
+        </div>
+      </div>
     </PageContainer>
   );
 }
