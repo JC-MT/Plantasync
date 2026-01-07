795ccc3ee11321d2104cec5a7095862a83a81ec5
Jan Matias <jan@thebeyondgroup.la>
2026-01-07 02:51:06 -0500

    chore: remove deprecated routes

--- app/routes/auth/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { Outlet } from "react-router";
-export default function Layout() {
-  return (
-    <section className="max-w-96 m-auto px-4 py-10">
-      <Outlet />
-    </section>
-  );
-}
